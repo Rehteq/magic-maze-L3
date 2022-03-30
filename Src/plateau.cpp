b2beeb7fe5ebd0072f064dbe1656d1cc9345d430
amatheo <59826260+amatheo@users.noreply.github.com>
2022-03-30 14:56:30 +0200

    added plateau class

---
+++ Src/plateau.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mathéo Auer on 30/03/2022.
+//
+
+#include "plateau.h"
---
+++ Src/plateau.h
@@ -0,0 +1,18 @@
+//
+// Created by Mathéo Auer on 30/03/2022.
+//
+
+#ifndef SRC_PLATEAU_H
+#define SRC_PLATEAU_H
+#include <vector>
+#include "tuile.h"
+using namespace MMaze;
+class Plateau {
+public:
+private:
+    std::vector<Tuile> m_tuiles
+
+};
+
+
+#endif //SRC_PLATEAU_H
